96c079245296dcf952165e5739bb21ce4d4ffab4
Sindre Sorhus <sindresorhus@gmail.com>
2018-03-19 17:28:54 +0700

    Cleanup

--- linter.py
+++ linter.py
@@ -1,14 +1,14 @@
 from SublimeLinter.lint import NodeLinter
 
 class XO(NodeLinter):
-    npm_name = 'xo'
-    cmd = ('xo', '--stdin', '--reporter', 'compact', '--filename', '@')
-    regex = (
-        r'^.+?: line (?P<line>\d+), col (?P<col>\d+), '
-        r'(?:(?P<error>Error)|(?P<warning>Warning)) - '
-        r'(?P<message>.+)'
-    )
-    defaults = {
-        'selector': 'source.js - meta.attribute-with-value',
-        'disable_if_not_dependency': True
-    }
+	npm_name = 'xo'
+	cmd = ('xo', '--stdin', '--reporter', 'compact', '--filename', '@')
+	regex = (
+		r'^.+?: line (?P<line>\d+), col (?P<col>\d+), '
+		r'(?:(?P<error>Error)|(?P<warning>Warning)) - '
+		r'(?P<message>.+)'
+	)
+	defaults = {
+		'selector': 'source.js - meta.attribute-with-value',
+		'disable_if_not_dependency': True
+	}
