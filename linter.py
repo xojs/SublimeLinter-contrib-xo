d6a8b995f2a1b069729f07ef43b966b2f15fd3b3
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-28 00:53:20 +0700

    Support the new SublimeLinter `code` property

--- linter.py
+++ linter.py
@@ -2,11 +2,12 @@ from SublimeLinter.lint import NodeLinter
 
 class XO(NodeLinter):
 	npm_name = 'xo'
-	cmd = ('xo', '--stdin', '--reporter', 'compact', '--filename', '@')
+	cmd = ('xo', '--stdin', '--reporter', 'compact', '--filename', '${file}')
 	regex = (
 		r'^.+?: line (?P<line>\d+), col (?P<col>\d+), '
 		r'(?:(?P<error>Error)|(?P<warning>Warning)) - '
 		r'(?P<message>.+)'
+		r' \((?P<code>.+)\)$'
 	)
 	defaults = {
 		'selector': 'source.js - meta.attribute-with-value',
