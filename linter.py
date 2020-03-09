c6a66c7e9ec634a941bc2ab6b3c705080f04be72
herr kaste <herr.kaste@gmail.com>
2020-03-09 14:38:34 +0100

    Implement automatic selector if plugins are used (#20)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- linter.py
+++ linter.py
@@ -1,4 +1,22 @@
+import os
 from SublimeLinter.lint import NodeLinter
+
+# TODO: Properly export these in SL core: https://github.com/SublimeLinter/SublimeLinter/issues/1713
+from SublimeLinter.lint.linter import PermanentError
+from SublimeLinter.lint.base_linter.node_linter import read_json_file
+
+
+STANDARD_SELECTOR = 'source.js'
+PLUGINS = {
+	'eslint-plugin-html': 'text.html',
+	'eslint-plugin-json': 'source.json',
+	'eslint-plugin-markdown': 'text.html.markdown',
+	'eslint-plugin-svelte3': 'text.html',
+	'eslint-plugin-vue': 'text.html.vue',
+	'@typescript-eslint/parser': 'source.ts',
+}
+OPTIMISTIC_SELECTOR = ', '.join({STANDARD_SELECTOR} | set(PLUGINS.values()))
+
 
 class XO(NodeLinter):
 	npm_name = 'xo'
@@ -10,6 +28,48 @@ class XO(NodeLinter):
 		r' \((?P<code>.+)\)$'
 	)
 	defaults = {
-		'selector': 'source.js - meta.attribute-with-value',
+		'selector': OPTIMISTIC_SELECTOR,
 		'disable_if_not_dependency': True
 	}
+
+	def run(self, cmd, code):
+		self.ensure_plugin_installed()
+		return super().run(cmd, code)
+
+	def ensure_plugin_installed(self) -> bool:
+		# If the user changed the selector, we take it as is.
+		if self.settings['selector'] != OPTIMISTIC_SELECTOR:
+			return True
+
+		# Happy path.
+		if self.view.match_selector(0, STANDARD_SELECTOR):
+			return True
+
+		# If we're here we must be pessimistic.
+
+		# The `project_root` has the relevant `package.json` file colocated.
+		# If we fallback to a global installation there is no `project_root`,
+		# t.i. no auto-selector in that case as well.
+		project_root = self.context.get('project_root')
+		if project_root:
+			# We still need to be careful, in case SublimeLinter deduced a `project_root`
+			# without checking for the `package.json` explicitly. Basically, a
+			# happy path for SublimeLinter core.
+			manifest_file = os.path.join(project_root, 'package.json')
+			try:
+				manifest = read_json_file(manifest_file)
+			except Exception:
+				pass
+			else:
+				defined_plugins = PLUGINS.keys() & (
+					manifest.get('dependencies', {}).keys()
+					| manifest.get('devDependencies', {}).keys()
+				)
+				selector = ', '.join(PLUGINS[name] for name in defined_plugins)
+				if selector and self.view.match_selector(0, selector):
+					return True
+
+		# Indicate an error which usually can only be solved by changing
+		# the environment. Silently, do not notify and disturb the user!
+		self.notify_unassign()
+		raise PermanentError()
